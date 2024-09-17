15ee7b16553f191cbbf9dbd62cfd2aa1f7d18cef
Eleazar Pestano <eleazar.pestano@hotmail.com>
2024-09-17 15:07:02 -0700

    Adding collapsible feature to navigation bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-const slider = document.querySelector(".slider");
-const output = document.querySelector(".slider-output");
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
 
-slider.oninput = function () {
-  output.innerText = this.value;
-};
+// const slider = document.querySelector(".slider");
+// const output = document.querySelector(".slider-output");
+// slider.oninput = function () {
+//   output.innerText = this.value;
+// };
