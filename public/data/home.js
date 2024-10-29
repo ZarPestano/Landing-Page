4c18e2ed6df0a52bf800431c8c8ca6ce722ad806
Eleazar Pestano <eleazar.pestano@hotmail.com>
2024-10-29 11:02:48 -0700

    Creating hard-coded data for pages

---
+++ public/data/home.js
@@ -0,0 +1,34 @@
+export default {
+  title: "About Me",
+  image: "../images/profilePicture.jpg",
+  technologies: ["HTML", "CSS", "JavaScript", "TypeScript"],
+  libraries: ["React", "Chakra-UI"],
+  miscTechnologies: [
+    "React-Query",
+    "Zustand",
+    "React-Router",
+    "Axios",
+    "Git/GitHub",
+  ],
+  description: [
+    "Hello! I'm Eleazar - though most people know me as Zar. With a\
+    background in mechanical engineering, I discovered my love for\
+    programming during my studies at BCIT and while working in the field.\
+    From automating mechanical systems to creating simulations in Maple\
+    15, Visual Basic, MATLAB, LabVIEW and C, I was always drawn to\
+    problem-solving through code. My passion for web development was\
+    sparked when friends in software development introduced me to the\
+    world of building interactive and dynamic applications - turning\
+    simple lines of code into something impactful felt like magic.",
+
+    "My goal is to become a full-stack software developer and explore the\
+    endless possibilities the world of software development has to offer.\
+    I'm excited to continue learning and pushing my skills further.",
+
+    "When I'm not coding, you can find me exploring the outdoors through\
+    hiking, weightlifting at the gym, hitting the slopes snowboarding, or\
+    playing sports like basketball, volleyball and badminton. I'm also a\
+    fan of video games and can often be caught belting out karaoke tunes\
+    at Filipino parties or soundproof karaoke rooms.",
+  ],
+};
---
+++ public/data/projects.js
@@ -0,0 +1,56 @@
+export default {
+  count: 3,
+  next: null,
+  previous: null,
+  results: [
+    {
+      id: 1,
+      name: "Moshified Cloud Hosting",
+      category: "Front-End",
+      technology: ["HTML", "CSS", "JavaScript"],
+      library: ["AOS"],
+      image: "../images/moshifywebproject-cropped-1000x563.png",
+      link: "https://moshifywebproject.netlify.app/",
+      description:
+        "A mobile-first, fully responsive cloud hosting website designed to deliver a seamless user experience across\
+         all devices. Built with HTML and CSS, the site incorporates media queries for optimized viewing at different\
+         screen widths. Scroll animations are powered by the AOS scroll library, enhancing engagement and providing\
+         dynamic visual cues. JavaScript is used to implement collapsible menus, ensuring intuitive navigation on smaller\
+         screens. Moshified effectively balances design and functionality, creating an engaging experience for users\
+         exploring cloud hosting solutions.",
+    },
+    {
+      id: 2,
+      name: "Asteroids Game Recreation",
+      category: "Front-End",
+      technology: ["JavaScript"],
+      library: ["n/a"],
+      image: "../images/asteroidsgameproject-cropped-1000x563.png",
+      link: "https://asteroidsgameproject.netlify.app",
+      description:
+        "A modern web-based recreation of the classic 1979 Asteroids arcade game by Atari Inc., blending nostalgic\
+         gameplay with updated browser based technologies. This project was built to be both accessible and enjoyable,\
+         with intuitive controls for a seamless user experience. Players navigate with arrow keys and fire with the\
+         space bar to challenge the high score, combining simple mechanics with dynamic visuals. The game highlights\
+         programming skills in JavaScript, demonstrating interactive functionaility and real-time user feedback through\
+         score-tracking and responsive controls.",
+    },
+    {
+      id: 3,
+      name: "GameHub",
+      category: "Full-Stack",
+      technology: ["React", "TypeScript"],
+      library: ["Chakra-UI", "Axios", "React-Query", "Zustand", "React-Router"],
+      image: "../images/gamehubwebproject-cropped-1000x563.png",
+      link: "https://gamehubwebproject.netlify.app",
+      description:
+        "A GameHub is a modern video game discovery web application designer for effortless exploration of gaming\
+         content through a sleek, responsive interface. Powered by React, this project pulls real-time data from an online\
+         third-party API, offering users up-to-date game details. GameHub leverages Chakra-UI for an accessible,\
+         visually-appealing layout and uses React-Query and Axios to efficiently manage server-side data. Client state\
+         management is streamlined with Zustand, while React-Router enables smooth, intuitive navigation. This project\
+         demonstrates expertise in building efficient interactive applications that enhance the user experience through\
+         a sophisticated, modular technology stack.",
+    },
+  ],
+};
